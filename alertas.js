1e3a2eda300ed7611c5d97ed7a3bb61a58dc88f0
YeinerA23 <ypalacioscasas08@gmail.com>
2026-07-28 11:33:39 -0500

    Delete alertas.js

--- alertas.js
+++
@@ -1,7 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
-  <rect width="64" height="64" rx="14" fill="#2E2440"/>
-  <path fill="#8E7CC3" d="M32 10 C25 8 18 13 19 21 C15 22 12 28 15 33 C11 37 13 44 19 45 C18 52 24 58 32 55 C40 58 46 52 45 45 C51 44 53 37 49 33 C52 28 49 22 45 21 C46 13 39 8 32 10 Z"/>
-  <circle cx="25" cy="29" r="2.5" fill="#F3F0F7"/>
-  <circle cx="39" cy="29" r="2.5" fill="#F3F0F7"/>
-  <path d="M25 39 Q32 44 39 39" stroke="#F3F0F7" stroke-width="2.2" fill="none" stroke-linecap="round"/>
-</svg>
